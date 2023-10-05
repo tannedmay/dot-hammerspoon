33e02a4f361e2fdbd1b972922ada94f3e605da4a
tannedmay <github@aliasemail.xyz>
2023-10-05 23:05:27 +0530

    New hammerspoon chnages

--- init.lua
+++ init.lua
@@ -12,11 +12,11 @@ Install=spoon.SpoonInstall
 
 Install:andUse("Hammer",
                {
-                 config = { auto_reload_config = true },
-                 -- hotkeys = {
-                 --   config_reload = {hyper, "r"},
-                 --   toggle_console = {hyper, "y"}
-                 -- },
+                 config = { auto_reload_config = false },
+                 hotkeys = {
+                   config_reload = {hyper, "r"},
+                   toggle_console = {hyper, "y"}
+                 },
                  start = true
                }
 )
@@ -58,7 +58,8 @@ Install:andUse("KSheet",
 
 Install:andUse("TimeMachineProgress",
                {
-                 start = true,
+                 start = false,
+                 disable = true
                }
 )
 
@@ -66,7 +67,7 @@ Install:andUse("TimeMachineProgress",
 Install:andUse("EjectMenu", {
                  config = {
                    eject_on_lid_close = true,
-                   eject_on_sleep = false,
+                   eject_on_sleep = true,
                    show_in_menubar = true,
                    notify = true,
                  },
@@ -87,7 +88,7 @@ Install:andUse("HeadphoneAutoPause",
 
 Install:andUse("Seal",
                {
-                 hotkeys = { show = { {"alt"}, "space" } },
+                 hotkeys = { show = { hyper, "space" } },
                  fn = function(s)
                    s:loadPlugins({"apps", "calc", "screencapture", "useractions"})
                    s.plugins.useractions.actions =
@@ -111,3 +112,77 @@ Install:andUse("FadeLogo",
                  start = true
                }
 )
+
+local laptopScreen = "Color LCD"
+local Monitor = "LG"
+
+-- Define position values that don't exist by default in hs.layout.*
+local positions = {
+  leftTop = {x=0, y=0, w=0.5, h=0.5},
+  leftBottom = {x=0, y=0.5, w=0.5, h=0.5},
+  rightTop = {x=0.5, y=0, w=0.5, h=0.5},
+  rightBottom = {x=0.5, y=0.5, w=0.5, h=0.5}
+  -- left = {x=0, y=0, w=0.5, h=1}
+  -- right = {x=0.5, y=0, w=0.5, h=1}
+}
+
+local layoutDualScreen = {
+  {"GoLand", nil, Monitor, positions.left, nil, nil},
+  {"Firefox", nil, Monitor, positions.right, nil, nil},
+  {"ClickUp", nil, Monitor, positions.right, nil, nil},
+  {"Discord", nil, Monitor, positions.right, nil, nil},
+  {"Sublime Text", nil, Monitor, positions.left, nil, nil},
+  {"Sublime Merge", nil, Monitor, positions.left, nil, nil},
+  {"iTerm", nil, Monitor, positions.rightBottom, nil, nil},
+}
+
+local layoutSingleScreen = {
+  {"GoLand", nil, laptopScreen, hs.layout.maximized, nil, nil},
+  {"Firefox", nil, laptopScreen, hs.layout.maximized, nil, nil},
+  {"ClickUp", nil, laptopScreen, hs.layout.maximized, nil, nil},
+  {"Discord", nil, laptopScreen, hs.layout.maximized, nil, nil},
+  {"Sublime Text", nil, laptopScreen, hs.layout.maximized, nil, nil},
+  {"Sublime Merge", nil, laptopScreen, hs.layout.maximized, nil, nil},
+  {"iTerm", nil, laptopScreen, positions.rightBottom, nil, nil},
+}
+
+local appNames = {
+  "GoLand",
+  "Firefox",
+  "ClickUp",
+  "Discord",
+  "Sublime Text",
+  "Sublime Merge",
+  "iTerm"
+}
+
+local function launchApps()
+  for i, appName in ipairs(appNames) do
+    hs.application.launchOrFocus(appName)
+  end
+end
+
+local menu = hs.menubar.new()
+local function setSingleScreen()
+  menu:setTitle("🖥1")
+  menu:setTooltip("Single Screen Layout")
+  hs.layout.apply(layoutSingleScreen)
+end
+
+local function setDualScreen()
+  menu:setTitle("🖥2")
+  menu:setTooltip("Dual Screen Layout")
+  hs.layout.apply(layoutDualScreen)
+end
+
+local function enableMenu()
+  menu:setTitle("🖥")
+  menu:setTooltip("No Layout")
+  menu:setMenu({
+      { title = "Launch Apps", fn = launchApps },
+      { title = "Set Dual Screen Layout", fn = setDualScreen },
+      { title = "Set Single Screen Layout", fn = setSingleScreen },
+  })
+end
+
+enableMenu()
